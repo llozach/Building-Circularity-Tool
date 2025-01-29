956603db8cb352cc66f44855de759b0c5df6481e
llozach <llozach@ethz.ch>
2025-01-29 14:35:08 +0100

    Cleaned the database and modifiying the user input data so that product identifiers are unique

--- BCI-app/remove_data.py
+++ BCI-app/remove_data.py
@@ -1,9 +1,15 @@
 # python snippet that will remove all products with only zero entries in the table r_strategies
 
 import duckdb
+from pathlib import Path
+
+# Defining project paths
+
+app_dir = Path(__file__).parent
+data_dir = app_dir / "data"
 
 # replace path to building_data.db as needed. Connection to db from other users must be closed
-con = duckdb.connect('/Users/tlm/Documents/Building-Circularity-Tool/BCI-app/data/building_data.db')
+con = duckdb.connect(data_dir / "building_data.db")
 
 # query to fetch and display the products with only zero entries
 query = f"""SELECT * FROM r_strategies WHERE "Virgin"=0 and "Reused"=0 and "Recycled"=0 and "Repurposed"=0"""
